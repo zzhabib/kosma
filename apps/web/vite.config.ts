6d30db5a9dcfac8f389475729046c80f37259ec1
zzhabib <zach.habib16@gmail.com>
2026-05-01 22:16:57 -0500

    Setup github pages

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/kosma/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
